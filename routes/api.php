ad85911dceef91f4b1d8b256e05cbbf64b367ce5
mubaroksoki <81344306+mubaroksoki@users.noreply.github.com>
2021-09-06 08:18:04 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Routing\Route as RoutingRoute;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/me',[AuthController::class,'me']);
